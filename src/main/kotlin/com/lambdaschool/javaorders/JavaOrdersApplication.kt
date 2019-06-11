904614e6ce26ffe0914167a74e88193d36884fa5
heiligbasil <basilief@yahoo.com>
2019-06-11 13:30:26 -0700

    Basil Havens - Java_Orders

---
+++ src/main/kotlin/com/lambdaschool/javaorders/JavaOrdersApplication.kt
@@ -0,0 +1,12 @@
+package com.lambdaschool.javaorders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaOrdersApplication
+
+fun main(args: Array<String>)
+{
+    runApplication<JavaOrdersApplication>(*args)
+}
